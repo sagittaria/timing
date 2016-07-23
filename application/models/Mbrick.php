6af6dcc95fb8611676ae7c9fe23231d4f7bfe199
kasoya <shuyang_x@hotmail.com>
2016-07-23 14:11:48 +0800

    add func: check if this is your OWN block

--- application/models/Mbrick.php
+++ application/models/Mbrick.php
@@ -23,12 +23,23 @@ class Mbrick extends CI_Model {
 		$query = $this->db->select('brickId,brickStart,brickDuration,brickContent')
 		->order_by('brickId asc')
 		->where('blockId',$BlockId)
-		->limit(10)
-		->get('brick');
+		->get('brick',6);
 		return $query->result_array();
 	}
 	
-	public function MFshowMoreBricks(){
-		return "testString";
+	public function MFshowMoreBricks($blockId,$per_page,$num){
+		//1.检查是不是本人的block
+		$query = $this->db->select('builderId')
+		->where('blockId',$blockId)
+		->get('block',1);
+		$check = $query->result_array();
+		if($_SESSION['id']!=$check[0]['builderId'])
+			return;
+		
+		//2.处理正事
+		$info=array();
+		$info['bricks']=$query->result_array();
+		$info['total_rows']=183;
+		return $info;
 	}
 }
