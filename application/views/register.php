a849696e8324db5d83e99f2e24b472074019b71f
sagittaria <shuyang_x@hotmail.com>
2016-08-12 11:26:07 +0800

    add js validation before form-submit

--- application/views/register.php
+++ application/views/register.php
@@ -14,9 +14,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</script>
     </head>
   <body>
-    <div class="container">
+    <div class="container">    
     <?php echo form_open('Welcome/register','id="regForm" name="regForm" class="form-sign"'); ?>
-	<h4 class="form-sign-heading" style="text-align:center;">Sign up to Timing</h4>
+	<h4 class="form-sign-heading" style="text-align:center;">Sign up for Timing</h4>
 	<div class="form-group"><label for="builderUsername">Username</label>
 	<input id="builderUsername" type="text" name="builderUsername" value="<?php echo set_value('builderUsername'); ?>" class="form-control sign-up" required>
 		<?php echo form_error('builderUsername'); ?>
@@ -29,13 +29,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<input id="builderEmail" type="email" name="builderEmail" value="<?php echo set_value('builderEmail'); ?>" class="form-control sign-up" required>
 		<?php echo form_error('builderEmail'); ?>
 	</div>
-	<button type="submit" class="btn btn-success btn-block">Create an account</button>
+
+	<div class="alert alert-danger" style="display:none;"></div>
+
+	<a class="btn btn-success btn-block" onclick="signup()">Create an account</a>
 	<p style="text-align:center;margin-top:7px;">Already have one? <a href="<?php echo site_url('Welcome/index'); ?>">Sign in</a>.</p>
 	</form>
 
     </div> <!-- /container -->
 
-    <script src="<?php echo base_url('public/jquery.js');?>" type="text/javascript"></script>
-	<script src="<?php echo base_url('public/js/bootstrap.min.js');?>" type="text/javascript"></script>
+      <script src="<?php echo base_url('public/jquery.js');?>" type="text/javascript"></script>
+      <script src="<?php echo base_url('public/js/bootstrap.min.js');?>" type="text/javascript"></script>
+      <script>
+	  function signup(){
+		if(jsvalidate()){
+		  $('#regForm').submit();
+		}else{
+		  return;
+		}
+	  }
+	  function warn(msg){
+		$('.alert').hide();
+		$('.alert').html(msg).show();
+	  }
+	  function jsvalidate(){
+		var username = $('[name=builderUsername]').val().trim() || '';
+    		var password = $('[name=builderPassword]').val() || '';
+		var email = $('[name=builderEmail]').val();
+
+		if(username.length === 0){ warn('Username field required.');return false; }
+		if(username.length < 5){ warn('Username too short.');return false; }
+		if(password.length < 6){ warn('Password at least 6 chars.');return false; }
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    		if(!re.test(email)){ warn('incorrect Email address.'); return false;}
+
+		return true;
+		
+	  }
+      </script>
   </body>
 </html>
