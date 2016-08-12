d8f06ca64b1ecadccc68de66d0f5feb9d43e47b5
sagittaria <shuyang_x@hotmail.com>
2016-08-12 12:50:30 +0800

    modify view to auto fill Username once after signing up

--- application/views/login.php
+++ application/views/login.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="container">
     <?php echo form_open('Welcome/verifying','id="loginForm" name="loginForm" class="form-sign"'); ?>
 		<h4 class="form-sign-heading" style="text-align:center;">Sign in to Timing</h4>
-		<input type="text" name="builderUsername" value="<?php echo set_value('builderUsername'); ?>" class="form-control sign-in" placeholder="Username" required autofocus>
+		<input type="text" name="builderUsername" value="<?php if(isset($cookieUsername)){echo htmlspecialchars($cookieUsername);} ?><?php echo set_value('builderUsername'); ?>" class="form-control sign-in" placeholder="Username" required autofocus>
 		<input type="password" name="builderPassword" class="form-control sign-in" placeholder="Password" required>
 		<button type="submit" class="btn btn-primary btn-block">Continue</button>
 		<p style="text-align:center;">New to Timing? <a href="<?php echo site_url('Welcome/register'); ?>">Create an account</a>.</p>
