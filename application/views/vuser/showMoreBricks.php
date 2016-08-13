d23c94a7a1bdadfc7c4d70191b959d9f11f0a62c
sagittaria <shuyang_x@hotmail.com>
2016-08-13 15:24:40 +0800

    changing showMoreBricks page

--- application/views/vuser/showMoreBricks.php
+++ application/views/vuser/showMoreBricks.php
@@ -8,18 +8,27 @@
 		<script> $(function () {	$('#filterDate').datetimepicker({viewMode:'days', maxDate:moment().format('YYYY/MM/DD'), format:'YYYY/MM/DD'});	});	</script>
 		<input type="submit" value="Filter"> Tips: Leave it blank and press Filter to view all</form>
 		</div>
+
+	<div class="container">
+	  <div class="row">
+	    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
 		<span style="float:right;"><?php echo $tips; ?></span>
-	<table class="table table-striped table-bordered">
-	<tr class='success'><th>#</th><th>start</th><th>duration, min.</th><th>content</th><th>OPs</th></tr>
-		<?php foreach($bricks as $brick){ ?>
-			<tr><td><?php echo $brick['brickId']; ?></td>
-				<td><?php echo date('Y/m/d H:i',$brick['brickStart']); ?></td>
-				<td><?php echo $brick['brickDuration']; ?></td>
-				<td><?php echo $brick['brickContent']; ?></td>
-				<td><button class="btn btn-default btn-xs" onclick="if(confirm('Are you sure?')){deleteThisBrick(<?php echo $brick['brickId']; ?>)}">Del</button></td></tr>
-		<?php } ?>
-	</table>
-		
+		<table class="table table-striped table-bordered">
+		<tr class='success'><th>#</th><th>start</th><th>duration, min.</th><th>content</th><th>OPs</th></tr>
+			<?php $cnt=0; foreach($bricks as $brick){ ?>
+				<tr><td><?php /*echo $brick['brickId'];*/ echo ++$cnt;  ?></td>
+					<td><?php echo date('Y/m/d H:i',$brick['brickStart']); ?></td>
+					<td><?php echo $brick['brickDuration']; ?></td>
+					<td><?php echo $brick['brickContent']; ?></td>
+					<td><button class="btn btn-default btn-xs" onclick="if(confirm('Are you sure?')){deleteThisBrick(<?php echo $brick['brickId']; ?>)}">Del</button></td></tr>
+			<?php } ?>
+		</table>
+	    </div>
+	    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+		one more chart
+	    </div>
+	  </div><!--row-->
+	</div><!--container-->
 	<script>
 		function deleteThisBrick(brickId){
 			$.ajax({//异步删brick
