6411532e3d8f9c22419e73747957fcbd3f3227b5
sagittaria <shuyang_x@hotmail.com>
2016-08-27 11:59:29 +0800

    prepare to display manual

--- application/views/vuser/manual.php
+++ application/views/vuser/manual.php
@@ -1,1 +1,12 @@
-user-guide
+<div class="container">
+<?php
+  require 'Parsedown.php';
+  $docFile = @fopen('manual.md','r') or die('User guide is missing...');
+  $manual = fread('manual.md',filesize('manual.md'));
+  fclose($docFile);
+
+  $parser = new Parsedown();
+  echo $parser->text($manual);
+  
+?>
+</div>
