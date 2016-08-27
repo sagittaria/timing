4b702c52709a128e6afae2715c39ff411741fa20
sagittaria <shuyang_x@hotmail.com>
2016-08-27 12:11:02 +0800

    ready to display, but Manual is now unfinished

--- application/views/vuser/manual.php
+++ application/views/vuser/manual.php
@@ -1,8 +1,8 @@
 <div class="container">
 <?php
   require 'Parsedown.php';
-  $docFile = @fopen('manual.md','r') or die('User guide is missing...');
-  $manual = fread('manual.md',filesize('manual.md'));
+  $docFile = fopen("manual.md","r") or die('User guide is missing...');
+  $manual = fread($docFile,filesize("manual.md"));
   fclose($docFile);
 
   $parser = new Parsedown();
